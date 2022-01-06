2be085d1604c17bce682ac8f7fe69beefb75eff7
leticiaandrade <leticia933@gmail.com>
2022-01-06 19:19:30 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,23 @@
+/* eslint-disable array-callback-return */
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  // const compareID = (ids === species.id);
+  // console.log(animals);
+  const result = [];
+  if (ids === undefined) {
+    return result;
+  }
+  // console.log(ids);
+  species.filter((item) => {
+    ids.filter((itemProcurado) => {
+      if (item.id === itemProcurado) {
+        result.push(item);
+      }
+    });
+  });
+  return result;
 }
 
 module.exports = getSpeciesByIds;
