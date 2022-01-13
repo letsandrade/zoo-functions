f7a25f84ee1e7090ff9e19a8a0d16c84b7958a8f
leticiaandrade <leticia933@gmail.com>
2022-01-12 22:20:18 -0300

    requisito 2 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,8 +2,10 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const { species } = data;
-  species.forEach((item) => console.log(item));
+  // encontrar animal buscado
+  const chosenAnimal = species.find((searchedAnimal) => searchedAnimal.name === animal);
+  // retornar true ou false se todos os residentes daquela especie atenderem a condição
+  return chosenAnimal.residents.every((searchedAge) => searchedAge.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
-console.log(getAnimalsOlderThan('otters', 7));
