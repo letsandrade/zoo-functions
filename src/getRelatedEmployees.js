8f9d3d9621eb408c8f35307d3077f3d2e94253b1
leticiaandrade <leticia933@gmail.com>
2022-01-13 22:23:57 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  const searched = employees.find((item) => item.id === id);
+  return (searched.managers.length === 0);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const managesWho = employees.filter((item) => item.managers.includes(managerId));
+    const arrNames = [];
+    managesWho.forEach((element) => arrNames.push(`${element.firstName} ${element.lastName}`));
+    return arrNames;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
