4af108759a1867a513c08052205463dd17610fc7
leticiaandrade <leticia933@gmail.com>
2022-01-14 18:43:42 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,31 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  const iniciando = 'eu';
-  return iniciando;
+  const result = entrants.reduce((acc, curr) => {
+    if (curr.age < 18) {
+      acc.child += 1;
+    } else if (curr.age < 50) {
+      acc.adult += 1;
+    } else {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const totalEntrants = countEntrants(entrants);
+  const totalChild = (totalEntrants.child * prices.child);
+  const totalAdult = (totalEntrants.adult * prices.adult);
+  const totalSenior = (totalEntrants.senior * prices.senior);
+  const finalCost = (totalChild + totalAdult + totalSenior);
+  return finalCost;
 }
 
 module.exports = { calculateEntry, countEntrants };
