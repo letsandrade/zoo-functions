e0719b480aed53914e81cdec2d7ab0b1f3b2bfd4
leticiaandrade <leticia933@gmail.com>
2022-01-14 17:39:43 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
 
+function countAnimals(animal) {
+  // primeiro caso, se não houver parâmetro
+  if (animal === undefined) {
+    const allAnimals = species.reduce((accumulator, currAnimal) => {
+      accumulator[currAnimal.name] = currAnimal.residents.length;
+      return accumulator;
+    }, {});
+    return allAnimals;
+  }
+
+  if (animal.sex === undefined) {
+    const chosenAnimal = species.find(({ name }) => name === animal.specie);
+    const numberRes = chosenAnimal.residents.length;
+    return numberRes;
+  }
+  const genderResult = species.find(({ name }) => name === animal.specie).residents;
+  return genderResult.filter(({ sex }) => sex === animal.sex).length;
+}
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
 module.exports = countAnimals;
