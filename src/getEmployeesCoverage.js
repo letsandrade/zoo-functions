8963f85411cc9206e5e45aab733ef7e44bb10726
leticiaandrade <leticia933@gmail.com>
2022-01-15 04:54:37 -0300

    requisito 10 depois de um longo e tenebroso inverno

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,13 +2,45 @@ const data = require('../data/zoo_data');
 
 const { employees, species } = data;
 
-function getSpecies(searchedInfo) {
-  const searchedPerson = employees.find((person) => person.id === searchedInfo.id || person.firstName === searchedInfo.name || person.lastName === searchedInfo.name);
-  const animalsTutor = searchedPerson.responsibleFor;
-  const findAnimals = species.filter((item) => searchedPerson.includes(item.id));
-}
-function getEmployeesCoverage(employee = {}) {
-  
+function getSpecific(emp) {
+  const searched = employees
+    .find((p) => p.id === emp.id || p.firstName === emp.name || p.lastName === emp.name);
+  const animalsTutor = searched.responsibleFor;
+  const findAnimals = species.filter((item) => animalsTutor.includes(item.id));
+  const animalsNames = findAnimals.map((element) => element.name);
+  const animalsPlaces = findAnimals.map((place) => place.location);
+  const employeeObj = {
+    id: searched.id,
+    fullName: `${searched.firstName} ${searched.lastName}`,
+    species: animalsNames,
+    locations: animalsPlaces,
+  };
+  return employeeObj;
 }
 
+function getAllEmployees() {
+  const allEmployees = employees.map((item) => {
+    const checkAnimals = species.filter((element) => item.responsibleFor.includes(element.id));
+    return {
+      id: item.id,
+      fullName: `${item.firstName} ${item.lastName}`,
+      species: checkAnimals.map((animal) => animal.name),
+      locations: checkAnimals.map((element) => element.location),
+    };
+  });
+  return allEmployees;
+}
+
+function getEmployeesCoverage(obj = {}) {
+  if (Object.keys(obj).length === 0) {
+    return getAllEmployees();
+  }
+  const specificEmployee = employees
+    .find((i) => (i.id === obj.id || i.firstName === obj.name || i.lastName === obj.name));
+  if (!specificEmployee) {
+    throw new Error('Informações inválidas');
+  }
+  return getSpecific(obj);
+}
+console.log(getEmployeesCoverage());
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,19 +1,19 @@
 const data = require('../data/zoo_data');
 
-const { hours, species } = data;
+// const { hours, species } = data;
 
 function getSchedule(scheduleTarget) {
-  const week = Object.keys(hours)
+/*   const week = Object.keys(hours)
   console.log(week);
   const animals = species.map((item) => item.name);
   console.log(animals);
   const completeSchedule = {
-    
+
   };
   // primeiro caso, sem parâmetro
   //if (!scheduleTarget) {
-    
-  //}
+
+  //} */
 }
-console.log(getSchedule());
+// console.log(getSchedule());
 module.exports = getSchedule;
